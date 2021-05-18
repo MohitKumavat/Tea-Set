dfb095ade21b9f735c222c62a35c29810de9e83d
MohitKumavat <kumavatmohit007@gmail.com>
2021-05-18 13:45:19 -0400

    f commit

---
+++ Assets/Scripts/cubeMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeMovement: MonoBehaviour
+{
+    public float mSpeed = 10f;
+    Vector3 mVelocity;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        mVelocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.B))
+        {//for 
+            transform.Translate( Time.deltaTime * mSpeed,0f,0f);
+        }
+        if (Input.GetKey(KeyCode.N))
+        {
+            transform.Translate(-Time.deltaTime * mSpeed, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.G))
+        {
+            transform.Translate(0f, mSpeed * Time.deltaTime, 0f);
+        }
+        if (Input.GetKey(KeyCode.H))
+        {
+            transform.Translate(0f, -mSpeed * Time.deltaTime, 0f);
+        }
+    }
+}
---
+++ Assets/Scripts/moveTeapot.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveTeapot : MonoBehaviour {
+    public float mSpeed=10;
+    Vector3 mVelocity;
+    public ParticleSystem liq;
+
+	// Use this for initialization
+	void Start () {
+        liq.Stop();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        mVelocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.X)){//for 
+            mVelocity.z = 1.0f;
+        } else if (Input.GetKey(KeyCode.Z)){
+            mVelocity.z = -1.0f;
+        }
+
+        if (Input.GetKey(KeyCode.L)){
+            mVelocity.x = 1.0f;
+        }
+        else if (Input.GetKey(KeyCode.J)){
+            mVelocity.x= -1.0f;
+        }
+        if (Input.GetKey(KeyCode.I))
+        {
+            mVelocity.y = 1.0f;
+        }
+        else if (Input.GetKey(KeyCode.K))
+        {
+            mVelocity.y = -1.0f;
+        }
+        transform.Translate(mVelocity.normalized * Time.deltaTime*mSpeed);
+        if (Input.GetKey(KeyCode.E)) {
+            transform.Rotate(0.0f,0.0f, 40 * Time.deltaTime);
+            liq.Stop();
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0.0f, 0.0f, -40 * Time.deltaTime);
+            liq.Play();
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            transform.Rotate(0.0f, 40 * Time.deltaTime, 0.0f);
+        }
+        else if (Input.GetKey(KeyCode.T))
+        {
+            transform.Rotate(0.0f, -40 * Time.deltaTime, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            transform.Rotate( 40 * Time.deltaTime,0.0f, 0.0f);
+        }
+        else if (Input.GetKey(KeyCode.C))
+        {
+            transform.Rotate(-40 * Time.deltaTime, 0.0f, 0.0f);
+        }
+
+    }
+}
